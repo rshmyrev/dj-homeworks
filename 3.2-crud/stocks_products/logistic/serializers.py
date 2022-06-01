1d3a2d75bf39513b5d859d62b354a1061b8e13b7
Roman Shmyrev <rshmyrev@gmail.com>
2022-06-01 14:38:49 +0300

    hw3.2 CRUD

--- 3.2-crud/stocks_products/logistic/serializers.py
+++ 3.2-crud/stocks_products/logistic/serializers.py
@@ -1,17 +1,24 @@
+from logistic.models import Product, Stock, StockProduct
 from rest_framework import serializers
 
 
 class ProductSerializer(serializers.ModelSerializer):
-    # настройте сериализатор для продукта
-    pass
+    class Meta:
+        model = Product
+        fields = '__all__'
 
 
 class ProductPositionSerializer(serializers.ModelSerializer):
-    # настройте сериализатор для позиции продукта на складе
-    pass
+    class Meta:
+        model = StockProduct
+        fields = ['product', 'quantity', 'price']
 
 
 class StockSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Stock
+        fields = '__all__'
+
     positions = ProductPositionSerializer(many=True)
 
     # настройте сериализатор для склада
@@ -26,6 +33,12 @@ class StockSerializer(serializers.ModelSerializer):
         # здесь вам надо заполнить связанные таблицы
         # в нашем случае: таблицу StockProduct
         # с помощью списка positions
+        for position in positions:
+            product = position.pop('product')
+            StockProduct.objects.update_or_create(
+                stock=stock, product=product,
+                defaults=position,
+            )
 
         return stock
 
@@ -39,5 +52,11 @@ class StockSerializer(serializers.ModelSerializer):
         # здесь вам надо обновить связанные таблицы
         # в нашем случае: таблицу StockProduct
         # с помощью списка positions
+        for position in positions:
+            product = position.pop('product')
+            StockProduct.objects.update_or_create(
+                stock=stock, product=product,
+                defaults=position,
+            )
 
         return stock
