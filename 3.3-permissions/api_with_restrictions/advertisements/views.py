c791dda15d6ac198194fe8131b5992c3927d492b
Roman Shmyrev <rshmyrev@gmail.com>
2022-06-02 13:36:15 +0300

    hw3.3 done permission for create, update, delete

--- 3.3-permissions/api_with_restrictions/advertisements/views.py
+++ 3.3-permissions/api_with_restrictions/advertisements/views.py
@@ -1,10 +1,15 @@
 from django_filters import rest_framework as filters
-from rest_framework.permissions import IsAuthenticated
+from rest_framework.permissions import IsAuthenticated, BasePermission
 from rest_framework.viewsets import ModelViewSet
 
 from .filters import AdvertisementFilter
 from .models import Advertisement
 from .serializers import AdvertisementSerializer
+
+
+class IsCreatorPermission(BasePermission):
+    def has_object_permission(self, request, view, obj):
+        return request.user == obj.creator
 
 
 class AdvertisementViewSet(ModelViewSet):
@@ -17,6 +22,6 @@ class AdvertisementViewSet(ModelViewSet):
 
     def get_permissions(self):
         """Получение прав для действий."""
-        if self.action in ["create", "update", "partial_update"]:
-            return [IsAuthenticated()]
+        if self.action in ["create", "update", "partial_update", "delete"]:
+            return [IsAuthenticated(), IsCreatorPermission()]
         return []
